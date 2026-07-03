f20ad1cb18dfdff04bef7677a7c9d8f6169a03ec
ErickLimonG <tolsericko@hotmail.com>
2026-07-02 19:49:39 -0600

    wip

--- mc-server/mc-start.sh
+++ mc-server/mc-start.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 LOCAL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$LOCAL_DIR/utils/confirmation_prompt.sh" || exit 1
 source "$LOCAL_DIR/utils/sudo.sh" || exit 1
 
-start_minecraft_server() {
+_start_minecraft_server() {
 	(
 		local MIN_MEMORY="${1:-1024}M"
 		local MAX_MEMORY="${2:-2048}M"
@@ -18,12 +18,12 @@ start_minecraft_server() {
 	)
 }
 
-eula_prompt() {
+_eula_prompt() {
 	local SERVER_DIR="$LOCAL_DIR/server"
 	local EULA="$SERVER_DIR/eula.txt"
 
 	if grep -q "eula=true" "$EULA"; then
-		exit 2 # eula already true
+		return 2 # eula already true
 	elif grep -q "eula=false" "$EULA"; then
 		echo "Would you like to accept the Minecraft eula?, Y/N"
 		echo "You can read it here https://account.mojang.com/documents/minecraft_eula"
@@ -33,42 +33,48 @@ eula_prompt() {
 		fi
 	else
 		echo "Error: $EULA not found"
-		exit 1
+		return 1
 	fi
 
 }
 
-add_server_properties() {
+_add_server_properties() {
 	local SERVER_CONFIG="$LOCAL_DIR/server/server.properties"
 	local KEY=$1
 	local VALUE=$2
 	echo "$KEY=$VALUE" >>"$SERVER_CONFIG"
 }
 
-configure_minecraft_rcon() {
+_configure_minecraft_rcon() {
 	#https://minecraft.wiki/w/RCON
 	local SERVER_CONFIG="$LOCAL_DIR/server/server.properties"
 	local PROMPT="Would you like to enable RCON?"
 	if confirmation_prompt "$PROMPT"; then
-		add_server_properties "enable-rcon" "true"
-		add_server_properties "rcon.password" 1
-		add_server_properties "rcon.port" 25575
-		add_server_properties "broadcast-rcon-to-ops" "false"
+		_add_server_properties "enable-rcon" "true"
+
+		read -rsp "Please set a password" RCON_PASSWORD
+		_add_server_properties "rcon.password" "$RCON_PASSWORD"
+
+		read -rp "Select port from 1 to 65535, press Enter to select default (25575)" PORT
+		_add_server_properties "rcon.port" "${PORT:-25575}"
+		
+		_add_server_properties "broadcast-rcon-to-ops" "false"
 	fi
 }
 
 main() {
 	local MIN_MEMORY="$1"
 	local MAX_MEMORY="$2"
-	start_minecraft_server "$MIN_MEMORY" "$MAX_MEMORY"
 
-	eula_prompt
+	_start_minecraft_server "$MIN_MEMORY" "$MAX_MEMORY"
+
+	_eula_prompt
 	EULA_PROMPT_EXIT_CODE=$?
 
-	configure_minecraft_rcon
+	_configure_minecraft_rcon
 	# Only works on first install of the server
 	if $EULA_PROMPT_EXIT_CODE; then
-		start_minecraft_server "$MIN_MEMORY" "$MAX_MEMORY"
+		_start_minecraft_server "$MIN_MEMORY" "$MAX_MEMORY"
 	fi
 }
 
